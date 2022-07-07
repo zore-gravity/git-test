78e4ebba62a0c6a22800146608f89d16554cb279
lcc <2427707577@qq.com>
2022-07-07 21:18:58 +0800

    hot-fix  commit1

--- src/main/java/com/lcc/git/GitTest.java
+++ src/main/java/com/lcc/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git22");
         System.out.println("hello git33");
+        System.out.println("hello git44");
     }
 }
